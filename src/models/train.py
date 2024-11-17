97fc5a5714035235448be11faea61d70eb02e48e
Jokey14 <467440721@qq.com>
2024-11-17 23:04:59 +0800

    optim utils, optim console output, optim dataloader

--- src/models/train.py
+++ src/models/train.py
@@ -1,42 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import random_split, DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from src.data.data_preprocessing import CustomDataset1
 from src.models.model import Type2Model
 import os
 import time
+from src.utils import DataAgent, set_seed, save_model_incrementally
 
 writer = SummaryWriter(os.path.join(os.path.dirname(__file__), '../../logs'))
-
-dataset = CustomDataset1(3)
-batch_size = 8
-
-# 设置数据集大小
-total_size = len(dataset)
-train_size = int(0.8 * total_size)  # 80% 用于训练
-val_size = int(0.1 * total_size)    # 10% 用于验证
-test_size = total_size - train_size - val_size  # 剩余 10% 用于测试
-
-# 划分数据集
-train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
-train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
-
-
-class DataAgent:
-    def __init__(self, num_blocks=12, batch_size=8):
-        self.dataset = CustomDataset1(num_blocks)
-        self.total_size = total_size = len(self.dataset)
-        self.train_size = train_size = int(0.8 * total_size)  # 80% 用于训练
-        self.val_size = val_size = int(0.1 * total_size)    # 10% 用于验证
-        self.test_size = total_size - train_size - val_size  # 剩余 10% 用于测试
-        self.train_dataset, self.val_dataset, self.test_dataset = random_split(self.dataset, [train_size, val_size, test_size])
-    
-    def get_train_loader():
-        
 
 
 def log_dice_loss_with_logit(y_hat, y, ep=1e-8):
@@ -49,7 +20,7 @@ def log_dice_loss_with_logit(y_hat, y, ep=1e-8):
     dice_ce_loss = -1 * torch.log(dice) + ce_total_loss
     return dice_ce_loss
 
-def train(net, device, loss_fn=log_dice_loss_with_logit, offset=94, num_epochs=200):
+def train(net, device, train_loader, val_loader, loss_fn=log_dice_loss_with_logit, offset=94, num_epochs=200):
 
     # 初始化模型
     
@@ -69,7 +40,7 @@ def train(net, device, loss_fn=log_dice_loss_with_logit, offset=94, num_epochs=2
             l.backward()
             optimizer.step()
             running_loss += l.item()
-            if (i % (len(train_loader) // 10) == 0): print(f'epoch: {epoch+1} / {num_epochs}, batch: {i+1} / {len(train_loader)}, loss:{l}')
+            print(f'epoch: {epoch+1} / {num_epochs}, batch: {i+1} / {len(train_loader)}, loss:{l} ', end='\r')
         net.eval()
         val_loss = 0.0
         with torch.no_grad():
@@ -85,33 +56,14 @@ def train(net, device, loss_fn=log_dice_loss_with_logit, offset=94, num_epochs=2
         writer.add_scalar('Loss/val', val_loss/len(val_loader), epoch)
     # 保存模型
     print(f'train end, timer: {time.time() - timer_tik}')
-    save_model_incrementally(net)
-
-
-def save_model_incrementally(net, directory=os.path.dirname(__file__), base_name='net'):
-    # 确保目录存在
-    os.makedirs(directory, exist_ok=True)
-    
-    # 获取现有文件的编号
-    existing_files = [f for f in os.listdir(directory) if f.startswith(base_name) and f.endswith('.pth')]
-    
-    # 提取已有文件的编号
-    numbers = [int(f.split('_')[-1].split('.')[0]) for f in existing_files if '_' in f]
-    
-    # 确定下一个编号
-    next_number = max(numbers) + 1 if numbers else 1
-    
-    # 构建文件名
-    file_name = f"{base_name}_{next_number}.pth"
-    file_path = os.path.join(directory, file_name)
-    
-    # 保存模型
-    torch.save(net.state_dict(), file_path)
-    print(f"Model saved as {file_name}")
+    save_model_incrementally(net, os.path.join(os.path.dirname(__file__), '../../models'))
 
 
 if __name__ == "__main__":
+    set_seed(0)
     net = Type2Model()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net = net.to(device)
-    train(net, device, num_epochs=10)
+    if 'da' not in locals():
+        da = DataAgent(3)
+    train(net, device, da.get_train_loader(), da.get_val_loader(), num_epochs=10)
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,11 @@
 import torch
 import torch.nn.functional as F
+import random
+import numpy as np
+from src.data.data_preprocessing import CustomDataset1
+from torch.utils.data import random_split, DataLoader
+import os
+
 
 def expand_image_to_center(image, target_size=(512, 512)):
     # 获取原图像尺寸
@@ -15,3 +21,56 @@ def expand_image_to_center(image, target_size=(512, 512)):
     padded_image = F.pad(image, (pad_w, pad_w, pad_h, pad_h), mode='constant', value=0)
 
     return padded_image
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+class DataAgent:
+    def __init__(self, num_blocks=12):
+        self.dataset = CustomDataset1(num_blocks)
+        self.total_size = total_size = len(self.dataset)
+        self.train_size = train_size = int(0.8 * total_size)  # 80% 用于训练
+        self.val_size = val_size = int(0.1 * total_size)    # 10% 用于验证
+        self.test_size = test_size = total_size - train_size - val_size  # 剩余 10% 用于测试
+        self.train_dataset, self.val_dataset, self.test_dataset = random_split(self.dataset, [train_size, val_size, test_size])
+    
+    
+    def get_train_loader(self, batch_size=8):
+        return DataLoader(self.train_dataset, batch_size, shuffle=True)
+    
+    
+    def get_val_loader(self, batch_size=8):
+        return DataLoader(self.val_dataset, batch_size, shuffle=False)
+    
+    
+    def get_test_loader(self, batch_size=8):
+        return DataLoader(self.test_dataset, batch_size, shuffle=False)
+    
+
+def save_model_incrementally(net, directory, base_name='net'):
+    # 确保目录存在
+    os.makedirs(directory, exist_ok=True)
+    
+    # 获取现有文件的编号
+    existing_files = [f for f in os.listdir(directory) if f.startswith(base_name) and f.endswith('.pth')]
+    
+    # 提取已有文件的编号
+    numbers = [int(f.split('_')[-1].split('.')[0]) for f in existing_files if '_' in f]
+    
+    # 确定下一个编号
+    next_number = max(numbers) + 1 if numbers else 1
+    
+    # 构建文件名
+    file_name = f"{base_name}_{next_number}.pth"
+    file_path = os.path.join(directory, file_name)
+    
+    # 保存模型
+    torch.save(net.state_dict(), file_path)
+    print(f"Model saved as {file_name}")
